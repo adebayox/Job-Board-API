6b4b633d379207e44a04ce31c7741a84edc3c235
mac <dadebayo200@gmail.com>
2022-10-11 13:26:07 +0100

    API ready

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser =  require('body-parser');
+
+//init the app
+const app = express()
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/job-board')
+mongoose.Promise = global.Promise
+
+app.use(bodyParser.json())
+
+//initiate routes
+app.use('/api', require('./routes/api'))
+
+//error handling middleware
+app.use(function (err, req, res, next) {
+    res.status(422).send({error: err.message})
+})
+
+
+app.listen(process.env.port || 4000, function () {
+    console.log('ready to accept request')
+});
